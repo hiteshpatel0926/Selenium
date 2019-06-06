d2d92fd111ed6b02f73c2efc49dfff4c2897407b
hiteshpa <hiteshpa@LIN30000206.corp.capgemini.com>
2019-06-06 10:01:55 +0530

    .

---
+++ E2EMaven/src/test/java/QA/E2EMaven/Test1.java
@@ -0,0 +1,12 @@
+package QA.E2EMaven;
+
+import org.testng.annotations.Test;
+
+ @Test
+public class Test1 {
+
+	  public static void main( String[] args )
+	    {
+	        System.out.println( "Hello World!" );
+	    }
+}
