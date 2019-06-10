479dc3908fe7324b46d595b124507510e3e41a9b
hiteshpa <hiteshpa@LIN30000206.corp.capgemini.com>
2019-06-10 10:09:38 +0530

    .

--- DemoMaven/src/test/java/com/tests/ScenarioTest1.java
+++ DemoMaven/src/test/java/com/tests/ScenarioTest1.java
@@ -1,17 +1,24 @@
 package com.tests;
+
+import org.apache.logging.log4j.LogManager;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
+import org.testng.log4testng.Logger;
 
 import com.pages.*;
 
 public class ScenarioTest1 extends TestBase
 {
+	
+	Logger logger = (Logger) LogManager.getLogger(ScenarioTest1.class.getSimpleName());
+	
 	@Test (priority = 0)
 	 public void Test1() throws Exception
 	{
 		driver.get("http://automationpractice.com/index.php");
 		ApDefaultHomePage signPage = PageFactory.initElements(driver, ApDefaultHomePage.class);
 		signPage.clickOnSingin();
+		logger.info("SignIn Button Click Successful");
 		
 		ApLoginPage loginpage = PageFactory.initElements(driver, ApLoginPage.class);
 		loginpage.setEmail("gtl_test@thegatewaycorp.com");
