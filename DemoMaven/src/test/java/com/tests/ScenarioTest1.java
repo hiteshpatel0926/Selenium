db7a3fdf87784fcb0a4bd5d8fd23285606897067
hiteshpa <hiteshpa@LIN30000206.corp.capgemini.com>
2019-06-10 10:34:51 +0530

    Log4j udpated.

--- DemoMaven/src/test/java/com/tests/ScenarioTest1.java
+++ DemoMaven/src/test/java/com/tests/ScenarioTest1.java
@@ -1,45 +1,50 @@
 package com.tests;
 
-import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.*;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
-import org.testng.log4testng.Logger;
-
 import com.pages.*;
 
 public class ScenarioTest1 extends TestBase
 {
 	
-	Logger logger = (Logger) LogManager.getLogger(ScenarioTest1.class.getSimpleName());
-	
+	public static Logger log = LogManager.getLogger(ScenarioTest1.class.getName());	
 	@Test (priority = 0)
 	 public void Test1() throws Exception
 	{
 		driver.get("http://automationpractice.com/index.php");
 		ApDefaultHomePage signPage = PageFactory.initElements(driver, ApDefaultHomePage.class);
 		signPage.clickOnSingin();
-		logger.info("SignIn Button Click Successful");
+		log.info("Signin Button Click Successful");
 		
 		ApLoginPage loginpage = PageFactory.initElements(driver, ApLoginPage.class);
 		loginpage.setEmail("gtl_test@thegatewaycorp.com");
 		loginpage.setPassword("Gtl@123");
 		loginpage.clickOnLoginButton();
+		log.info("Login Successful");
 		
 		ApHomePage womanTab = PageFactory.initElements(driver, ApHomePage.class);
 		womanTab.clickOnWomantab();
+		log.info("Clicking on Woman Tab Successful");
 		
 		ApWomantab womanTab1 = PageFactory.initElements(driver, ApWomantab.class);
 		womanTab1.clickOnTops();
+		log.info("Clicking on Tops inside Woman Tab Successful");
+		
 		
 		ApTopsTab womanTab2 = PageFactory.initElements(driver, ApTopsTab.class);
 		womanTab2.clickOnTshirts();
+		log.info("Clicking on Tshirts inside Woman Tab>>Tops Successful");
 		
 		ApClickonProduct product = PageFactory.initElements(driver, ApClickonProduct.class);
 		product.clickOnProduct();
+		log.info("Clicking on Product Successful");
 		
 		ApProductATC AddToCart = PageFactory.initElements(driver, ApProductATC.class);
 		AddToCart.ATC();
+		log.info("Product Added to Cart Successful");
 		AddToCart.verification();
+		log.info("Product verification Successful");
 		
 	}
 	
