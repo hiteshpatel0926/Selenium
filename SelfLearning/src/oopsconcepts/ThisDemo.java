382ccab1be53fb28dea1a3a9a99819dcbddab0d4
hiteshpatel0926 <40137107+hiteshpatel0926@users.noreply.github.com>
2019-06-13 15:00:58 +0530

    Class Updated

---
+++ SelfLearning/src/oopsconcepts/ThisDemo.java
@@ -0,0 +1,21 @@
+package oopsconcepts;
+
+public class ThisDemo {
+
+	int i=5;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ThisDemo d=new ThisDemo();
+		d.demo();
+		
+	}
+	
+	public void demo() {
+		
+		int i=7;
+		this.i=6;
+		System.out.println(i);
+		System.out.println(this.i);
+	}
+	
+}
